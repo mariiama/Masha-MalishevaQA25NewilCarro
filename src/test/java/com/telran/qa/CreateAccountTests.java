8db663cd201118be56d02de833b7fbac4c68f7d7
mariiama <mariiamalisheva@gmail.com>
2020-10-10 17:15:32 +0300

    +metod pause

--- src/test/java/com/telran/qa/CreateAccountTests.java
+++ src/test/java/com/telran/qa/CreateAccountTests.java
@@ -14,26 +14,31 @@ public class CreateAccountTests extends TestBase{
         }
     }
 
+
     @Test
     //click on signUp button
-
 //click submit button
-
-
-    public void testSignUp(){
+    public void testSignUp() throws InterruptedException {
         click(By.cssSelector("[href='/signup']"));
         Assert.assertTrue(isElementPresent(By.cssSelector("form.signup__fields")));
         //fill registration form
 
-        fillRegistrationForm("MA", "SHA", "foxqa25-3@qa.co", "FoXfOxFoX1");
-        fillRegistrationForm("SA","SHA","sasha1309@gmail.com","13FoXfOxFoX");
+        fillRegistrationForm("MA", "SHA", "foxqa25-4@qa.co", "FoXfOxFoX1");
+        fillRegistrationForm("SA","SHA","sasha1308@gmail.com","13FoXfOxFoX");
 
         click(By.cssSelector("#check_policy"));
+        pausa(3000);
+
         //click submit button
         submitForm();
         //check,login form displayed
         Assert.assertTrue(isLoginFromPresent());
     }
+
+    public void pausa(int millis) throws InterruptedException {
+        Thread.sleep(millis);
+    }
+
 
     public void fillRegistrationForm(String firstName, String secondName, String email, String password) {
         type(By.cssSelector("#first_name"), firstName);
