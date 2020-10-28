daadf2b3dcdda429378ebb75551cfd4df0f37192
mariiama <70857507+mariiama@users.noreply.github.com>
2020-10-28 16:07:59 +0200

    Create file logback

--- src/test/java/com/telran/qa/FW/ApplicationManager.java
+++ src/test/java/com/telran/qa/FW/ApplicationManager.java
@@ -42,4 +42,5 @@ public class ApplicationManager {
     public FooterHelper getFooter() {
         return footer;
     }
+
 }
--- src/test/java/com/telran/qa/FW/TestBase.java
+++ src/test/java/com/telran/qa/FW/TestBase.java
@@ -1,11 +1,16 @@
 package com.telran.qa.FW;
 
 import com.telran.qa.FW.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager();
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp()
@@ -13,11 +18,20 @@ public class TestBase {
         app.init();
     }
 
+    @BeforeMethod
+    public void startTest(Method m){
+        logger.info("Start test" + m.getName());
+    }
+
     @AfterSuite(enabled = false)
     public void tearDown()
     {
         app.stop();
     }
+
+    @AfterMethod
+    public void stopTest(Method m){
+        logger.info("Stop test" + m.getName());}
 
 
 }
